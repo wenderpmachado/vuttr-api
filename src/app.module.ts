f6179dcc92e6b1a04e237ec0bdf33287e5cf15d6
Wender Machado <wender.machado@rgsistemas.com.br>
2020-06-17 02:24:04 -0300

    :wrench: Configured mongoose + env example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './config/mongoose-config.service';
 import { ToolsModule } from './tools/tools.module';
 
 @Module({
-  imports: [ToolsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({ useClass: MongooseConfigService }),
+    ToolsModule
+  ],
   controllers: [],
   providers: [],
 })
