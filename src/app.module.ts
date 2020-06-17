9169e031a46c2d84443aa7b2eb8de330a9d84fa3
Wender Machado <wender.machado@rgsistemas.com.br>
2020-06-17 01:15:00 -0300

    :sparkles: Created tools module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ToolsModule } from './tools/tools.module';
 
 @Module({
-  imports: [],
+  imports: [ToolsModule],
   controllers: [],
   providers: [],
 })
